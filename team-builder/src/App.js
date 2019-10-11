04a81273aaebeca2976635ecf2f36bae092b39cd
Joshua Starling <joshstarling97@gmail.com>
2019-10-10 20:33:29 -0400

    project done, just trying stretch and gonna play with some styling

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./Form.js";
 
 function App() {
+  const [team, setTeam] = useState([]);
+  const [memberToEdit, setMemberToEdit] = useState({
+    name: "",
+    email: "",
+    role: ""
+  });
+
+  const editMember = memberToAdd => {
+    setMemberToEdit(memberToAdd);
+    console.log(memberToAdd);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {team.map((element, index) => {
+        return (
+          <div>
+            <p>{element.name}</p>
+            <p>{element.email}</p>
+            <p>{element.role}</p>
+
+            <button onClick={() => editMember(element)}>EDIT</button>
+          </div>
+        );
+      })}
+
+      <Form team={team} setTeam={setTeam} memberToEdit={memberToEdit} />
     </div>
   );
 }
